7d8f529910dac036997d7dc6fa81ca0002316b93
lizhiguang <2590238112@qq.com>
2021-02-28 21:59:55 +0800

    redis

--- spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/mapper/DepartmentMapper.java
+++ spring-boot-06-data-mybatis/src/main/java/com/atguigu/springboot/mapper/DepartmentMapper.java
@@ -22,7 +22,7 @@ public interface DepartmentMapper {
     int deleteDeptById(Integer id);
 
     @Options(useGeneratedKeys = true, keyProperty = "id")
-    @Insert("insert into department(department_name) values(#{departmentName})")
+    @Insert("insert into department(departmentName) values(#{departmentName})")
     int insertDept(Department department);
 
     @Update("update department set department_name=#{departmentName} where id=#{id}")
